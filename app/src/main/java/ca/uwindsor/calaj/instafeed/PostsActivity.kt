691b2037c9725fd928d36b3be020f8a6fc0c08e3
Jason Cala <calaj@uwindsor.ca>
2022-02-08 18:46:38 -0500

    Began querying for backend database data (gets data well)

--- app/src/main/java/ca/uwindsor/calaj/instafeed/PostsActivity.kt
+++ app/src/main/java/ca/uwindsor/calaj/instafeed/PostsActivity.kt
@@ -3,13 +3,35 @@ package ca.uwindsor.calaj.instafeed
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
+
+private const val TAG = "PostActivity"
 
 class PostsActivity : AppCompatActivity() {
+
+    private lateinit var firestoreDb: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        firestoreDb = FirebaseFirestore.getInstance()
+        val postsReference = firestoreDb.collection("posts")
+        postsReference.addSnapshotListener{ snapshot, exception ->
+            if (exception != null || snapshot == null){
+                Log.e(TAG, "Exception when querying posts", exception)
+                return@addSnapshotListener
+            }
+            for (document in snapshot.documents){
+                Log.i(TAG, "Document ${document.id}: ${document.data}")
+            }
+
+        }
+
+        //Making query to firestore to retrieve data
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
